da305730c5a05c9821e79a503222869ba478ce0e
alex.dzervanouski <alex.dzervanouski@exposit.com>
2019-10-09 13:07:25 +0300

    Remove redundant code

--- app/src/main/java/com/example/videoview/Consts.java
+++ app/src/main/java/com/example/videoview/Consts.java
@@ -7,6 +7,8 @@ import com.example.videoview.entity.videos.Videos;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+
 public class Consts {
 
     protected static final String YOUTUBE_WITH_TIME_URL = "https://www.youtube.com/watch?v=wIjB8jTDOZU&t=14s";
@@ -23,42 +25,40 @@ public class Consts {
     protected static final String VIDEO_URL = "VIDEO_URL";
     protected static final String VIDEO_TITLE = "VIDEO_TITLE";
 
-    //public static VideosDb convertVideosToVideosDb(Videos videos) {
-    //    YoutubeDb youtubeDb = new YoutubeDb(videos.getYoutube()
-    //                                              .getUrl());
-    //    VimeoDb vimeoDb = new VimeoDb(videos.getVimeo()
-    //                                        .getUrl());
-    //    DirectDb directDb = new DirectDb(videos.getDirect()
-    //                                           .getUrl());
-    //
-    //    return new VideosDb(youtubeDb, vimeoDb, directDb);
-    //}
-
-    public static List<VideosDb> convertVideosToVideosDb(Videos videos) {
+    public static List<VideosDb> convertVideosToVideosDb(@NonNull Videos videos) {
         List<VideosDb> videosDbList = new ArrayList<>();
-        for (int i = 0; i < videos.getYoutube()
-                                  .getUrl()
-                                  .size(); i++) {
-            VideosDb videosDb = new VideosDb(VideoType.YOUTUBE, videos.getYoutube()
-                                                                      .getUrl()
-                                                                      .get(i));
-            videosDbList.add(videosDb);
+        if (videos.getYoutube() != null && videos.getYoutube()
+                                                 .getUrl() != null) {
+            for (int i = 0; i < videos.getYoutube()
+                                      .getUrl()
+                                      .size(); i++) {
+                VideosDb videosDb = new VideosDb(VideoType.YOUTUBE, videos.getYoutube()
+                                                                          .getUrl()
+                                                                          .get(i));
+                videosDbList.add(videosDb);
+            }
         }
-        for (int i = 0; i < videos.getVimeo()
-                                  .getUrl()
-                                  .size(); i++) {
-            VideosDb videosDb = new VideosDb(VideoType.VIMEO, videos.getVimeo()
-                                                                    .getUrl()
-                                                                    .get(i));
-            videosDbList.add(videosDb);
+        if (videos.getVimeo() != null && videos.getVimeo()
+                                               .getUrl() != null) {
+            for (int i = 0; i < videos.getVimeo()
+                                      .getUrl()
+                                      .size(); i++) {
+                VideosDb videosDb = new VideosDb(VideoType.VIMEO, videos.getVimeo()
+                                                                        .getUrl()
+                                                                        .get(i));
+                videosDbList.add(videosDb);
+            }
         }
-        for (int i = 0; i < videos.getDirect()
-                                  .getUrl()
-                                  .size(); i++) {
-            VideosDb videosDb = new VideosDb(VideoType.DIRECT, videos.getDirect()
-                                                                     .getUrl()
-                                                                     .get(i));
-            videosDbList.add(videosDb);
+        if (videos.getYoutube() != null && videos.getYoutube()
+                                                 .getUrl() != null) {
+            for (int i = 0; i < videos.getDirect()
+                                      .getUrl()
+                                      .size(); i++) {
+                VideosDb videosDb = new VideosDb(VideoType.DIRECT, videos.getDirect()
+                                                                         .getUrl()
+                                                                         .get(i));
+                videosDbList.add(videosDb);
+            }
         }
         return videosDbList;
     }
--- app/src/main/java/com/example/videoview/db/VideosDb.java
+++ app/src/main/java/com/example/videoview/db/VideosDb.java
@@ -12,7 +12,7 @@ import androidx.room.TypeConverters;
 public class VideosDb {
 
     @PrimaryKey(autoGenerate = true)
-    int uidDdDdd;
+    int uid;
 
     @TypeConverters(VideoTypeConverter.class)
     VideoType type;
@@ -22,57 +22,4 @@ public class VideosDb {
         this.type = type;
         this.url = url;
     }
-
-    //@Nullable
-    //@Embedded
-    //private YoutubeDb youtubeDb;
-    //
-    //@Nullable
-    //@Embedded
-    //private VimeoDb vimeoDb;
-    //
-    //@Nullable
-    //@Embedded
-    //private DirectDb directDb;
-    //
-    //public int getUid() {
-    //    return uid;
-    //}
-    //
-    //public void setUid(int uid) {
-    //    this.uid = uid;
-    //}
-    //
-    //@Nullable
-    //public YoutubeDb getYoutubeDb() {
-    //    return youtubeDb;
-    //}
-    //
-    //public void setYoutubeDb(@Nullable YoutubeDb youtubeDb) {
-    //    this.youtubeDb = youtubeDb;
-    //}
-    //
-    //@Nullable
-    //public VimeoDb getVimeoDb() {
-    //    return vimeoDb;
-    //}
-    //
-    //public void setVimeoDb(@Nullable VimeoDb vimeoDb) {
-    //    this.vimeoDb = vimeoDb;
-    //}
-    //
-    //@Nullable
-    //public DirectDb getDirectDb() {
-    //    return directDb;
-    //}
-    //
-    //public void setDirectDb(@Nullable DirectDb directDb) {
-    //    this.directDb = directDb;
-    //}
-
-    //public VideosDb(@Nullable YoutubeDb youtubeDb, @Nullable VimeoDb vimeoDb, @Nullable DirectDb directDb) {
-    //    this.youtubeDb = youtubeDb;
-    //    this.vimeoDb = vimeoDb;
-    //    this.directDb = directDb;
-    //}
 }
